ca6f1538dddbfe69f31254dd8060ca0a61caf1fd
Alexandre Chabrolin <9203826+chabroA@users.noreply.github.com>
2022-07-03 12:10:18 +0200

    add IFrameEthereumConnector

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,213 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+import { IFrameEthereumProvider } from '@ledgerhq/iframe-provider';
+import { providers } from 'ethers';
+import { getAddress, hexValue } from 'ethers/lib/utils';
+import {
+  AddChainError,
+  ChainNotConfiguredError,
+  Connector,
+  ConnectorNotFoundError,
+  ResourceUnavailableError,
+  RpcError,
+  SwitchChainError,
+  UserRejectedRequestError,
+} from 'wagmi';
+
+import { normalizeChainId } from 'wagmi-core';
+
+type IFrameEthereumProviderOptions = ConstructorParameters<
+  typeof IFrameEthereumProvider
+>[0];
+
+export class IFrameEthereumConnector extends Connector<
+  IFrameEthereumProvider,
+  IFrameEthereumProviderOptions
+> {
+  readonly id = 'ledgerLive';
+  readonly name = 'Ledger Live';
+  readonly ready = true;
+
+  providerInstance?: IFrameEthereumProvider;
+
+  async connect({ chainId }: { chainId?: number } = {}) {
+    try {
+      const provider = await this.getProvider();
+      if (!provider) throw new ConnectorNotFoundError();
+
+      if (provider.on) {
+        provider.on('accountsChanged', this.onAccountsChanged);
+        provider.on('chainChanged', this.onChainChanged);
+      }
+
+      this.emit('message', { type: 'connecting' });
+
+      const account = await this.getAccount();
+      // Switch to chain if provided
+      let id = await this.getChainId();
+      let unsupported = this.isChainUnsupported(id);
+      if (chainId && id !== chainId) {
+        const chain = await this.switchChain(chainId);
+        id = chain.id;
+        unsupported = this.isChainUnsupported(id);
+      }
+
+      return { account, chain: { id, unsupported }, provider };
+    } catch (error) {
+      if (this.isUserRejectedRequestError(error))
+        throw new UserRejectedRequestError(error);
+      if ((error as RpcError).code === -32002)
+        throw new ResourceUnavailableError(error);
+      throw error;
+    }
   }
-  return a + b;
-};
+
+  async disconnect() {
+    const provider = await this.getProvider();
+    if (!provider?.removeListener) return;
+
+    provider.removeListener('accountsChanged', this.onAccountsChanged);
+    provider.removeListener('chainChanged', this.onChainChanged);
+  }
+
+  async getAccount() {
+    const provider = await this.getProvider();
+    if (!provider) throw new ConnectorNotFoundError();
+    const accounts = await provider.send('eth_requestAccounts');
+    // return checksum address
+    return getAddress(accounts[0] as string);
+  }
+
+  async getChainId() {
+    const provider = await this.getProvider();
+    if (!provider) throw new ConnectorNotFoundError();
+    return await provider.send('eth_chainId').then(normalizeChainId);
+  }
+
+  async getProvider() {
+    if (!this.providerInstance) {
+      this.providerInstance = new IFrameEthereumProvider(this.options);
+    }
+    return this.providerInstance;
+  }
+
+  async getSigner() {
+    const [provider, account] = await Promise.all([
+      this.getProvider(),
+      this.getAccount(),
+    ]);
+    return new providers.Web3Provider(
+      (provider as unknown) as providers.ExternalProvider
+    ).getSigner(account);
+  }
+
+  async isAuthorized() {
+    try {
+      const provider = await this.getProvider();
+      if (!provider) throw new ConnectorNotFoundError();
+      const accounts = await provider.send('eth_accounts');
+      const account = accounts[0];
+      return !!account;
+    } catch {
+      return false;
+    }
+  }
+
+  async switchChain(chainId: number) {
+    const provider = await this.getProvider();
+    if (!provider) throw new ConnectorNotFoundError();
+    const id = hexValue(chainId);
+
+    try {
+      await provider.send('wallet_switchEthereumChain', [{ chainId: id }]);
+
+      return (
+        this.chains.find(x => x.id === chainId) ?? {
+          id: chainId,
+          name: `Chain ${id}`,
+          network: `${id}`,
+          rpcUrls: { default: '' },
+        }
+      );
+    } catch (error) {
+      const chain = this.chains.find(x => x.id === chainId);
+      if (!chain) throw new ChainNotConfiguredError();
+
+      // Indicates chain is not added to provider
+      if (
+        (error as ProviderRpcError).code === 4902 ||
+        // Unwrapping for MetaMask Mobile
+        // https://github.com/MetaMask/metamask-mobile/issues/2944#issuecomment-976988719
+        (error as RpcError<{ originalError?: { code: number } }>)?.data
+          ?.originalError?.code === 4902
+      ) {
+        try {
+          await provider.send('wallet_addEthereumChain', [
+            {
+              chainId: id,
+              chainName: chain.name,
+              nativeCurrency: chain.nativeCurrency,
+              rpcUrls: [chain.rpcUrls.public ?? chain.rpcUrls.default],
+              blockExplorerUrls: this.getBlockExplorerUrls(chain),
+            },
+          ]);
+
+          return chain;
+        } catch (addError) {
+          if (this.isUserRejectedRequestError(addError))
+            throw new UserRejectedRequestError(error);
+          throw new AddChainError();
+        }
+      }
+
+      if (this.isUserRejectedRequestError(error))
+        throw new UserRejectedRequestError(error);
+      throw new SwitchChainError(error);
+    }
+  }
+
+  async watchAsset({
+    address,
+    decimals = 18,
+    image,
+    symbol,
+  }: {
+    address: string;
+    decimals?: number;
+    image?: string;
+    symbol: string;
+  }) {
+    const provider = await this.getProvider();
+    if (!provider) throw new ConnectorNotFoundError();
+
+    return await provider.send('wallet_watchAsset', [
+      {
+        type: 'ERC20',
+        options: {
+          address,
+          decimals,
+          image,
+          symbol,
+        },
+      },
+    ]);
+  }
+
+  protected onAccountsChanged = (accounts: string[]) => {
+    console.log({ length: accounts.length });
+    if (accounts.length === 0) this.emit('disconnect');
+    else this.emit('change', { account: getAddress(accounts[0]) });
+  };
+
+  protected onChainChanged = (chainId: number | string) => {
+    const id = normalizeChainId(chainId);
+    const unsupported = this.isChainUnsupported(id);
+    this.emit('change', { chain: { id, unsupported } });
+  };
+
+  protected isUserRejectedRequestError(error: unknown) {
+    return (error as ProviderRpcError).code === 4001;
+  }
+
+  protected onDisconnect = () => {
+    this.emit('disconnect');
+  };
+}
